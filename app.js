df2377b670bf749682dbc7eade2d3850493c0235
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2021-12-02 18:00:43 +0700

    #19 change https to http to support heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
+//const https = require("https");
+//const fs = require("fs");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -21,12 +21,26 @@ const { setReset, postReset } = require("./routes/reset-password");
 const app = express();
 const port = process.env.PORT;
 
-const httpsOpt = {
-  key: fs.readFileSync("./secure/cert.key"),
-  cert: fs.readFileSync("./secure/cert.pem"),
-};
+//https - not work on heroku, work on local
+// const httpsOpt = {
+//   key: fs.readFileSync("./secure/cert.key"),
+//   cert: fs.readFileSync("./secure/cert.pem"),
+// };
 
-app.enable("trust proxy");
+// app.enable("trust proxy");
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use((req, res, next) => {
+//     if (req.header("x-forwarded-proto") !== "https")
+//       res.redirect(`https://${req.header("host")}${req.url}`);
+//     else next();
+//   });
+// }
+
+// const server = https.createServer(httpsOpt, app).listen(port, () => {
+//   console.log("server running at " + port);
+// });
+//end of https
 
 app.set("port", port);
 app.set("views", __dirname + "/views");
@@ -48,14 +62,6 @@ app.use(flash());
 
 db.sequelize.sync({ force: true }).then(() => console.log("Resync db"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.header("x-forwarded-proto") !== "https")
-      res.redirect(`https://${req.header("host")}${req.url}`);
-    else next();
-  });
-}
-
 app.get("/", setHomePage);
 app.get("/sign-in", setSignIn);
 app.post("/sign-in", postSignIn);
@@ -71,6 +77,6 @@ app.get("/resend/:user_id", resend);
 app.get("/reset", setReset);
 app.post("/reset", postReset);
 
-const server = https.createServer(httpsOpt, app).listen(port, () => {
-  console.log("server running at " + port);
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
 });
--- mail/send_email.js
+++ mail/send_email.js
@@ -12,7 +12,7 @@ const sendEmail = (email, host, token) => {
     from: "no-reply@testlogin.com",
     to: email,
     subject: "Verify your sign-up mail",
-    html: `Hello, \n\nPlease verify your account by clicking this link:\n <a href="https:\/\/${host}\/confirmation\/${token}">https:\/\/${host}\/confirmation\/${token}</a>.\n`,
+    html: `Hello, \n\nPlease verify your account by clicking this link:\n <a href="http:\/\/${host}\/confirmation\/${token}">http:\/\/${host}\/confirmation\/${token}</a>.\n`,
   };
   mail.sendMail(mailOpt, (err, info) => {
     if (err) return 1;
