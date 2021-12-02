dcd2360bb43773cbc0ec3e4229d5987c9671f848
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2021-12-02 12:29:57 +0700

    #14 add dialectoptions

--- config/config.js
+++ config/config.js
@@ -30,11 +30,13 @@ module.exports = {
   },
   production: {
     use_env_variable: "HEROKU_POSTGRESQL_CHARCOAL_URL",
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       max: Number(process.env.POOL_MAX),
       min: Number(process.env.POOL_MIN),
